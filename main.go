be1cb8ebca8788e6db4b27fc739490899be5394e
BadgerOps <github@badgerops.net>
2024-03-28 16:50:30 +0100

    restructure, add log level handler

--- main.go
+++ main.go
@@ -16,6 +16,15 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+// set up log formatting and log level, I want actual timestamps
+func init() {
+	formatter := &logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+	}
+	logrus.SetFormatter(formatter)
+}
+
 // Use mutex to help mitigate collisions, I followed along with https://gobyexample.com/mutexes
 // Will add threading support next
 
@@ -30,7 +39,7 @@ func (dp *DownloadProgress) AddDownloadedBytes(n int) {
 	dp.mu.Lock()
 	defer dp.mu.Unlock()
 	dp.downloadedBytes += int64(n)
-	logrus.Trace("Downloaded bytes now equal to", dp.downloadedBytes)
+	//logrus.Trace("Downloaded bytes now equal to: ", dp.downloadedBytes)
 }
 
 func (dp *DownloadProgress) DownloadRate() float64 {
@@ -40,67 +49,6 @@ func (dp *DownloadProgress) DownloadRate() float64 {
 	logrus.Trace("current duration: ", duration)
 	logrus.Trace("Current bytes: ", dp.downloadedBytes)
 	return float64(dp.downloadedBytes) / duration
-}
-
-func main() {
-	app := &cli.App{
-		Name:  "downloader",
-		Usage: "Download things and save them in a dir",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "file, f",
-				Aliases:  []string{"f"},
-				Value:    "",
-				Usage:    "The path to the input `FILE` containing urls.",
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:     "outdir, d",
-				Aliases:  []string{"o"},
-				Value:    "./output",
-				Usage:    "The `DIRECTORY` where the files will be saved to.",
-				Required: false,
-			},
-		},
-		Action: func(c *cli.Context) error {
-
-			// create the output directory, if it does not exist already
-			outDir := c.String("outdir")
-			err := os.MkdirAll(outDir, 0755)
-			if err != nil {
-				return cli.Exit("Error creating output dir.", 1)
-			}
-			rawFile, err := readFile(c.String("file"))
-			if err != nil {
-				logrus.Error("Error reading input file:", err)
-				os.Exit(1)
-			}
-
-			dp := &DownloadProgress{
-				startTime:       time.Now(),
-				downloadedBytes: 0,
-			}
-
-			go func() {
-				ticker := time.NewTicker(5 * time.Second)
-				defer ticker.Stop()
-				for range ticker.C {
-					rate := int64(math.Round(dp.DownloadRate()))
-					logrus.Info("Current download rate: ", bytesToReadable(rate))
-				}
-			}()
-
-			getURL(rawFile, outDir, dp)
-			logrus.Info("Downloaded all files listed in:", c.String("file"))
-			return nil
-		},
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		cli.Exit(err.Error(), 1)
-		os.Exit(1)
-	}
 }
 
 func getURL(rawFile []string, outDir string, dp *DownloadProgress) {
@@ -133,7 +81,11 @@ func getURL(rawFile []string, outDir string, dp *DownloadProgress) {
 		}
 		defer out.Close()
 
-		// Read the response body in chunks, write each chunk to the file, and update dp.downloadedBytes
+		/*
+			Read the response body in chunks
+			write each chunk to output file
+			update dp.downloadedBytes
+		*/
 		buf := make([]byte, 1024)
 		for {
 			n, err := resp.Body.Read(buf)
@@ -188,21 +140,79 @@ func readFile(filePath string) ([]string, error) {
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
-
 	return lines, nil
 }
 
-func writeToDir(path string, data []byte) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return fmt.Errorf("failed to open file: %w", err)
-	}
-	defer file.Close()
+// Start up main func
+func main() {
+	var logLevel string
+	app := &cli.App{
+		Name:  "downloader",
+		Usage: "Download things and save them in a dir",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "file, f",
+				Aliases:  []string{"f"},
+				Value:    "",
+				Usage:    "The path to the input `FILE` containing urls.",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "outdir, d",
+				Aliases:  []string{"o"},
+				Value:    "./output",
+				Usage:    "The `DIRECTORY` where the files will be saved to.",
+				Required: false,
+			},
+			&cli.StringFlag{
+				Name:        "loglevel",
+				Value:       "info",
+				Usage:       "Set log level (debug, info, warn, error, fatal, panic)",
+				Destination: &logLevel,
+			},
+		},
+		Action: func(c *cli.Context) error {
 
-	_, err = file.Write(data)
-	if err != nil {
-		return fmt.Errorf("failed writing data to file: %w", err)
+			// create the output directory, if it does not exist already
+			outDir := c.String("outdir")
+			err := os.MkdirAll(outDir, 0755)
+			if err != nil {
+				return cli.Exit("Error creating output dir.", 1)
+			}
+			rawFile, err := readFile(c.String("file"))
+			if err != nil {
+				logrus.Error("Error reading input file:", err)
+				os.Exit(1)
+			}
+			level, err := logrus.ParseLevel(c.String("loglevel"))
+			if err != nil {
+				return err
+			}
+			logrus.SetLevel(level)
+
+			dp := &DownloadProgress{
+				startTime:       time.Now(),
+				downloadedBytes: 0,
+			}
+
+			go func() {
+				ticker := time.NewTicker(5 * time.Second)
+				defer ticker.Stop()
+				for range ticker.C {
+					rate := int64(math.Round(dp.DownloadRate()))
+					logrus.Info("Current download rate: ", bytesToReadable(rate))
+				}
+			}()
+
+			getURL(rawFile, outDir, dp)
+			logrus.Info("Downloaded all files listed in:", c.String("file"))
+			return nil
+		},
 	}
 
-	return nil
+	err := app.Run(os.Args)
+	if err != nil {
+		cli.Exit(err.Error(), 1)
+		os.Exit(1)
+	}
 }
